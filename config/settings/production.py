8a3374812ddd93a689c32732c4311dfc1d57d847
Anurag Goel <anurag@render.com>
2019-07-11 11:58:25 -0700

    Use RENDER_EXTERNAL_HOSTNAME

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[env('RENDER_APP_HOSTNAME', default='')])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[env('RENDER_EXTERNAL_HOSTNAME', default='')])
 
 # DATABASES
 # ------------------------------------------------------------------------------
